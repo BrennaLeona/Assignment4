a0c323048436c0bf835e36dd3139131debe84ac9
Brenna Fisher <2.bfisher@gmail.com>
2024-01-31 13:48:17 -0700

    test

--- Program.cs
+++ Program.cs
@@ -6,11 +6,14 @@
         Console.WriteLine("Welcome to TicTacToe!");
 
         //Create game board array
+        string[] gameBoard = new string[9];
 
         //Ask players for their choice & update game board
         Console.WriteLine("Where would you like to play? Pick a number 1-9.");
         int choice = Console.ReadLine();
 
+
+        //Ask plaers for their choice & update game board
 
         //Print board by calling method in TicTacTools
 

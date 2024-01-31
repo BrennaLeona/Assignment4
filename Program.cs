4580e2268ddf7e2b9a73a7c3f95f362b8f4d46c6
Brenna Fisher <2.bfisher@gmail.com>
2024-01-31 13:35:56 -0700

    comment requirements

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to TicTacToe!"); //Welcome the user
+
+        //Create game board array
+
+        //Ask plaers for their choice & update game board
+
+        //Print board by calling method in TicTacTools
+
+        //Check for winner by calling method in TicTacTools & notify players who won
+    }
+}

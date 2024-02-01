47d78b53ae8aac44bbd8ab66ef4a1976f84ec14a
jnelly17 <jaketnelson617@gmail.com>
2024-02-01 14:16:18 -0700

    print method format

--- TicTacTools.cs
+++ TicTacTools.cs
@@ -9,9 +9,13 @@ namespace Assignment4
     internal class TicTacTools
     {
 
-        public void printBoard()//gameBoard array as param
+        public void printBoard(string[] gameBoard)//gameBoard array as param
         {
-
+            Console.WriteLine($" {gameBoard[0]} | {gameBoard[1]} | {gameBoard[2]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {gameBoard[3]} | {gameBoard[4]} | {gameBoard[5]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {gameBoard[6]} | {gameBoard[7]} | {gameBoard[8]} ");
         }
 
         public void winnerName()

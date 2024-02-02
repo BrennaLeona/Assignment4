afef0c1e7273d5c68c3a90120268f3cb659e4a6d
jnelly17 <jaketnelson617@gmail.com>
2024-02-02 14:05:26 -0700

    comments

--- TicTacTools.cs
+++ TicTacTools.cs
@@ -9,8 +9,9 @@ namespace Assignment4
     internal class TicTacTools
     {
 
-        public void printBoard(string[] gameBoard)//gameBoard array as param
+        public void printBoard(string[] gameBoard) //gameBoard array as parameter
         {
+            //prints column by column with corresponding gameBoard array
             Console.WriteLine($" {gameBoard[0]} | {gameBoard[1]} | {gameBoard[2]} ");
             Console.WriteLine("---|---|---");
             Console.WriteLine($" {gameBoard[3]} | {gameBoard[4]} | {gameBoard[5]} ");
@@ -20,7 +21,7 @@ namespace Assignment4
 
         public string GetWinner(string[] gameBoard)
         {
-            
+            //checks for three in a row vertically (columns)
             for (int i = 0; i < 9; i += 3)
             {
                 if (gameBoard[i] == gameBoard[i + 1] && gameBoard[i + 1] == gameBoard[i + 2])
@@ -29,7 +30,7 @@ namespace Assignment4
                 }
             }
 
-            
+            //checks for three in a row horizontally (rows)
             for (int i = 0; i < 3; i++)
             {
                 if (gameBoard[i] == gameBoard[i + 3] && gameBoard[i + 3] == gameBoard[i + 6])
@@ -38,7 +39,7 @@ namespace Assignment4
                 }
             }
 
-           
+            //checks for three in a row diagonally
             if (gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8])
             {
                 return gameBoard[0];
